fd8e8a53d2cf774a8239e0cb8289c566329d02ab
Helarman <helarman@bk.ru>
2025-08-20 15:37:54 +0300

    fix copy

--- app/(protected)/(confirmed)/(platform)/wallets/page.tsx
+++ app/(protected)/(confirmed)/(platform)/wallets/page.tsx
@@ -276,10 +276,40 @@ const handleTransfer = async () => {
     }
   };
 
-  const copyToClipboard = (text: string) => {
-    navigator.clipboard.writeText(text);
-    toast.success('Copied to clipboard');
-  };
+const copyToClipboard = (text: string) => {
+  const textArea = document.createElement('textarea');
+  textArea.value = text;
+  
+  textArea.style.position = 'fixed';
+  textArea.style.top = '0';
+  textArea.style.left = '0';
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+  textArea.style.padding = '0';
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+  textArea.style.background = 'transparent';
+  textArea.style.opacity = '0';
+  textArea.style.pointerEvents = 'none';
+  textArea.style.zIndex = '-1';
+  
+  document.body.appendChild(textArea);
+  textArea.select();
+  textArea.setSelectionRange(0, 99999);
+  try {
+    const successful = document.execCommand('copy');
+    document.body.removeChild(textArea);
+    
+    if (successful) {
+      toast.success('Copied');
+    } else {
+      throw new Error('Failed copy');
+    }
+  } catch (error) {
+    document.body.removeChild(textArea);
+  }
+};
 
   const toggleAccountStatus = async (account: AccountEntity) => {
     try {
